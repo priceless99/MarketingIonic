ce6495f3eb786b443f7ce6e4538ebcba76cd065a
priceless99 <44872655+priceless99@users.noreply.github.com>
2019-08-07 18:14:14 -0500

    stuff is working now, upgraded ionic native

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import { EmailComposer } from '@ionic-native/email-composer/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { SMS } from '@ionic-native/sms/';
+import { SMS } from '@ionic-native/sms/ngx';
 import { AndroidPermissions } from '@ionic-native/android-permissions/ngx';
 
 @NgModule({
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RestapiService } from '../restapi.service';
-import { SMS } from '@ionic-native/sms/';
+import { SMS } from '@ionic-native/sms/ngx';
 import { CognitoService } from '../cognito.service';
 import { Router } from '@angular/router';
 import { AlertController } from '@ionic/angular';
