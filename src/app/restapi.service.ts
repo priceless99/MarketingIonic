d33bce52988ca844a602ede09e897d3c02fdc0b6
priceless99 <44872655+priceless99@users.noreply.github.com>
2019-08-14 17:14:29 -0500

    testing get and post lambda function

--- src/app/restapi.service.ts
+++ src/app/restapi.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { CognitoService } from './cognito.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -36,6 +37,23 @@ export class RestapiService {
     date: '2020-10-10',
     frequency: 'One Time'
   }];
+  
+
+  public messageTest = {
+    input: '',
+    text: false,
+    email: false,
+    push: false,
+    sendNow: false,
+    sendLater: false,
+    date: '',
+    time: '',
+    frequency: '',
+    subject: ''
+  } 
+
+
+ params = new  HttpParams().set('httpMethod', "GET").set('TableName', "testTable");
 
 
 
@@ -82,6 +100,93 @@ export class RestapiService {
 
     })
   }
+
+ 
+
+
+  getTest(params) {
+
+    let myHeaders = new HttpHeaders({
+      "Content-Type": "application/json"
+     });
+
+
+    const httpOptions = {
+      headers: { 'Content-Type': 'application/json' },
+      params
+  };
+
+
+    this.http.get('https://i7pvoxtvci.execute-api.us-east-1.amazonaws.com/default/lambda-microservice',
+    httpOptions)
+     .subscribe( response => {
+      console.log("get success: ", response);
+    }, err => {
+      console.log("get error: ", err);
+    });
+    console.log(httpOptions)
+  }
+
+  testPost() {
+
+    console.log('button works')
+
+     const httpOptions = {
+      headers: { 'Content-Type': 'application/json' }
+  };
+
+   let Item = {
+    
+   'TableName' : "testTable",
+   "Item" : { 
+     "ID" : "185",
+     "Name" : "gallardo",
+     "Speed" : "fast"
+      }
+    }
+  console.log("postdata: ", Item);
+  this.http.post('https://i7pvoxtvci.execute-api.us-east-1.amazonaws.com/default/lambda-microservice', JSON.stringify(Item),
+    httpOptions)
+  .subscribe( response => {
+    console.log("post success: ", response);
+  }, err => {
+    console.log("post error: ", err);
+  });
+  
+
+}
+
+
+
+
+//   testPost() {
+
+//     console.log('button works')
+
+//     let myHeaders = new HttpHeaders({
+//       "Content-Type": "application/json",
+//      });
+
+
+//    let item = {
+
+//    'BusinessId' : 11,
+//    'customers': "badbunny@CC",
+//    'messages' : this.messageTest,
+//     }
+//   console.log("postdata: ", item);
+// // https://499adbe4a1.execute-api.us-east-2.amazonaws.com/dev/postToTable
+//   this.http.post('https://s4i85hc772.execute-api.us-east-1.amazonaws.com/default/Tier1MarketingPOST', JSON.stringify(item),
+//   {headers : myHeaders}
+//   )
+//   .subscribe( response => {
+//     console.log("post success: ", response);
+//   }, err => {
+//     console.log("post error: ", err);
+//   });
+  
+
+// }
 
 
 
